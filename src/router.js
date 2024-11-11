6a361e9cb68eae672bcd96053f93b78b5cd8a9cf
sanha <heftycornerstone@gmail.com>
2024-11-11 17:59:51 +0900

    location-replace version

--- src/router.js
+++ src/router.js
@@ -1,49 +1,23 @@
-const ROUTE_PARAMETER_REGEXP = /:(\w+)/g;
-const URL_REGEXP = '([^\\/]+)';
-
 function createRouter() {
     const routes = [];
 
     const router = {
-        navigate(fragment) {
-            window.location.hash = fragment;
+        navigate(fragment, replace = false) {
+            if (replace) {
+                const href = window.location.href.replace(window.location.hash, "#" + fragment);
+                window.location.replace(href);
+            } else {
+                window.location.hash = fragment;
+            }
         },
         addRoute(fragment, component) {
-            const params = [];
-            const parsedFragment = fragment.replace(ROUTE_PARAMETER_REGEXP, (_, paramName) => {
-                params.push(paramName); 
-                return URL_REGEXP;
-            }).replace(/\//g, "\\/");
-
-            routes.push({
-                fragmentRegExp: new RegExp(`^${parsedFragment}$`),
-                component,
-                params, });
+            routes.push({ fragment, component });
             return this;
         },
         start() {
-            const getUrlParams = (route, hash) => {
-                const params = {};
-                const matches = hash.match(route.fragmentRegExp);
-
-                matches.shift();
-                matches.forEach((paramValue, i)=>{
-                    const paramName = route.params[i];
-                    params[paramName] = paramValue;
-                });
-
-                return params;
-            }
-
             const checkRoutes = () => {
-                const currentRoute = routes.find(route => route.fragmentRegExp.test(window.location.hash));
-
-                if(currentRoute.params.length) {
-                    const urlParams = getUrlParams(currentRoute, window.location.hash);
-                    currentRoute.component(urlParams);
-                } else {
-                    currentRoute.component();
-                }
+                const currentRoute = routes.find(route => route.fragment === window.location.hash);
+                currentRoute.component();
             }
 
             window.addEventListener('hashchange', checkRoutes);
--- src/script.js
+++ src/script.js
@@ -5,18 +5,18 @@ const container = document.querySelector('main');
 const pages = {
     home: () => container.innerText = 'home page',
     beef: () => container.innerText = 'beef',
-    dish: (params) => container.innerText = `beef ${params.dish}`,
+    dish: () => container.innerText = 'stew',
 }
 
 const router = createRouter();
 
 window.addEventListener("click", event => {
     if (event.target.matches("[data-navigate]")) {
-        router.navigate(event.target.dataset.navigate);
+        router.navigate(event.target.dataset.navigate, true);
     }
 });
 
 router.addRoute('#/', pages.home)
     .addRoute('#/beef', pages.beef)
-    .addRoute('#/beef/:dish', pages.dish)
+    .addRoute('#/stew', pages.dish)
     .start();
